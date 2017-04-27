546739fbc9ea3ac48c31c20ea57d88f41609460b
Henri Mikkonen <henri.mikkonen@iki.fi>
2017-04-27 15:25:36 +0300

    Update run-shibboleth.sh

--- container-scripts/run-shibboleth.sh
+++ container-scripts/run-shibboleth.sh
@@ -6,7 +6,7 @@ export JETTY_HOME=/opt/jetty/
 export JETTY_BASE=/opt/iam-jetty-base/
 export PATH=$PATH:$JAVA_HOME/bin
 
-cp -r /opt/shibboleth-idp/overlay/conf/* /opt/shibboleth-idp/conf
+cp -Lr /opt/shibboleth-idp/overlay/conf/* /opt/shibboleth-idp/conf
 
 sed -i "s/^-Xmx.*$/-Xmx$JETTY_MAX_HEAP/g" /opt/iam-jetty-base/start.ini
 
